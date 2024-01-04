c69e4ed4200f90fbb2b77b2feedc2bb4e10dd618
Simon Conrad <simondbconrad@gmail.com>
2024-01-04 11:39:55 -0500

    fix database routing issue

--- backend/routes/entryRoutes.js
+++ backend/routes/entryRoutes.js
@@ -1,9 +1,14 @@
-const express = require('express')
-const router = express.Router()
-const { getEntries, postEntry, updateEntry, deleteEntry } = require('../controllers/entryController')
+const express = require("express");
+const router = express.Router();
+const {
+  getEntries,
+  postEntry,
+  updateEntry,
+  deleteEntry,
+} = require("../controllers/entryController");
 
-router.route('/').get(getEntries).post(postEntry)
+router.route("/").get(getEntries).post(postEntry);
 
-router.route('/:id').put(updateEntry).delete(deleteEntry)
+router.route("/:id").put(updateEntry).delete(deleteEntry);
 
-module.exports = router
+module.exports = router;
