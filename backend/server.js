f61298dca3d1b98e2d0f312f9a0ccd1a607cbb3e
Simon Conrad <simondbconrad@gmail.com>
2024-01-18 12:14:48 -0500

    configure cors middleware

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const colors = require("colors");
 const dotenv = require("dotenv").config();
+const cors = require("cors");
+
 const { errorHandler } = require("./middleware/errorMiddleware");
 const connectDB = require("./config/db");
+
 const port = process.env.PORT;
+const droplet = process.env.DROPLET_IP;
 
 connectDB();
 
@@ -12,10 +16,21 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  next();
-});
+const corsOptionsLocal = {
+  origin: "http://localhost:3000",
+  methods: "GET, PUT, POST, DELETE",
+  credentials: true,
+};
+
+const corsOptionsDroplet = {
+  origin: `http://${droplet}`,
+  methods: "GET",
+  credentials: true,
+};
+
+const corsOptions = [corsOptionsLocal, corsOptionsDroplet];
+
+app.use(cors(corsOptions));
 
 app.use("/api/entries", require("./routes/entryRoutes"));
 
