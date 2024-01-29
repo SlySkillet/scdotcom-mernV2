b7520a2f19038fb0e3752306dcdb386d58836889
Simon Conrad <simondbconrad@gmail.com>
2024-01-29 12:27:49 -0500

    working navbar

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,7 +1,30 @@
 import "./nav-styles.css";
+import { Link } from "react-router-dom";
 
 const NavBar = function () {
-  return <div className="nav-container">suh?</div>;
+  return (
+    <div className="nav-container">
+      <div className="brand">S.C.</div>
+      <div className="links">
+        <Link className="nav-link" to="/">
+          Home
+        </Link>
+        <Link className="nav-link" to="/">
+          Projects
+        </Link>
+        <Link className="nav-link" to="/">
+          About
+        </Link>
+        <Link className="nav-link" to="/">
+          Resume
+        </Link>
+        <div className="nav-spacer">|</div>
+        <Link className="nav-link" to="/blog">
+          Blog
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default NavBar;
