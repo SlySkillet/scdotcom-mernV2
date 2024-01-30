3d626c08809d4eba030b6da3b0cc277b7d75ddc8
Simon Conrad <simondbconrad@gmail.com>
2024-01-29 19:54:53 -0500

    font touch up

--- frontend/src/components/blog/imagesDirectory.js
+++ frontend/src/components/blog/imagesDirectory.js
@@ -1,7 +1,8 @@
-import { FaEarlybirds } from "react-icons/fa";
+// import { FaEarlybirds } from "react-icons/fa";
+import { SiNewyorktimes } from "react-icons/si";
 
 const imagesDirectory = {
-  1: <FaEarlybirds />,
+  1: <SiNewyorktimes />,
   2: "https://res.cloudinary.com/ddgt67wcb/image/upload/v1702915981/EarthFromApollo17-58b849523df78c060e68ca36_mywt67.jpg",
 };
 
