4f2c68a47891524c499c00f4637a2d8a7440a26d
Simon Conrad <simondbconrad@gmail.com>
2024-01-30 12:10:33 -0500

    preliminary images for entries

--- frontend/src/components/blog/imagesDirectory.js
+++ frontend/src/components/blog/imagesDirectory.js
@@ -1,9 +1,16 @@
 // import { FaEarlybirds } from "react-icons/fa";
 import { SiNewyorktimes } from "react-icons/si";
+import { GiKiwiBird } from "react-icons/gi";
+import { SiGoogleearthengine } from "react-icons/si";
 
 const imagesDirectory = {
-  1: <SiNewyorktimes />,
-  2: "https://res.cloudinary.com/ddgt67wcb/image/upload/v1702915981/EarthFromApollo17-58b849523df78c060e68ca36_mywt67.jpg",
+  nyt: <SiNewyorktimes />,
+  blue_marble:
+    "https://res.cloudinary.com/ddgt67wcb/image/upload/v1702915981/EarthFromApollo17-58b849523df78c060e68ca36_mywt67.jpg",
+  sam_altman_1:
+    "https://res.cloudinary.com/ddgt67wcb/image/upload/v1706632913/sam_altman_t8nyya.jpg",
+  kiwi: <GiKiwiBird />,
+  google_earth: <SiGoogleearthengine />,
 };
 
 export default imagesDirectory;
