a1fb8098daa7b43c06269a359030d1fffa2aa026
Simon Conrad <simondbconrad@gmail.com>
2024-02-01 15:36:51 -0500

    create projects page containers

--- frontend/src/components/projects/projects.jsx
+++ frontend/src/components/projects/projects.jsx
@@ -1,5 +1,12 @@
+import "./projects-styles.css";
+
 const Projects = function () {
-  return <p>projects page suhn</p>;
+  return (
+    <div className="projects-container">
+      <h2 className="projects-page-title">Projects</h2>
+      <div className="main-projects-container">main container</div>
+    </div>
+  );
 };
 
 export default Projects;
