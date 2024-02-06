b5a34f008891a5e9ff38838dd10fc5b32f20254a
Simon Conrad <simondbconrad@gmail.com>
2024-02-06 11:58:45 -0500

    working title page

--- frontend/src/components/home/HomePage.jsx
+++ frontend/src/components/home/HomePage.jsx
@@ -1,12 +1,21 @@
 import "./home-styles.css";
+import { FaCode } from "react-icons/fa";
 
 function HomePage() {
   return (
     <div className="home-container">
       <div className="about-container">
-        <div className="name-container">
-          <h1 className="about-content name">Simon</h1>
-          <h1 className="about-content name">Conrad</h1>
+        <div className="box-left">
+          <div className="name-box">
+            <h1 className="name">Simon</h1>
+            <h1 className="name">Conrad</h1>
+          </div>
+          <FaCode className="decoration" />
+          <h5 className="title">Software Engineer</h5>
+          <p className="tech-list">
+            Python | Javascript | React | Django | FastAPI | SQL | Docker
+          </p>
+          <FaCode className="decoration" />
         </div>
         <img
           alt="simon"
