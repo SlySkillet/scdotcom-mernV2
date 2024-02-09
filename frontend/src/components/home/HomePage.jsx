bd7e28a466e6782f7807f6e631b19ccbb8a77ea8
Simon Conrad <simondbconrad@gmail.com>
2024-02-08 23:29:30 -0500

    create footer container

--- frontend/src/components/home/HomePage.jsx
+++ frontend/src/components/home/HomePage.jsx
@@ -2,6 +2,7 @@ import "./home-styles.css";
 import TitleSection from "./home-components/TitleSection";
 import BioContent from "./home-components/BioContent";
 import FeaturedProject from "./home-components/FeaturedProject";
+import Footer from "./home-components/Footer";
 
 function HomePage() {
   return (
@@ -9,6 +10,7 @@ function HomePage() {
       <TitleSection />
       <BioContent />
       <FeaturedProject />
+      <Footer />
     </div>
   );
 }
