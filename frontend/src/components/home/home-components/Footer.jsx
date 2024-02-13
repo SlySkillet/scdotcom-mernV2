851d79c396270925e9b1f88e92cb9b0f8d00723f
Simon Conrad <simondbconrad@gmail.com>
2024-02-13 08:26:51 -0500

    create footer

--- frontend/src/components/home/home-components/Footer.jsx
+++ frontend/src/components/home/home-components/Footer.jsx
@@ -1,9 +1,32 @@
 import "../home-styles.css";
+import { Link } from "react-router-dom";
+import { SiLinkedin, SiGithub } from "react-icons/si";
 
 function Footer() {
   return (
     <div className="footer-container">
-      <p>footer</p>
+      <p className="thanks">Thank you for visiting!</p>
+      <Link to="/contact" className="contact-me">
+        Contact me
+      </Link>
+      <div className="footer-link-box">
+        <Link
+          className="footer-link"
+          to="https://www.linkedin.com/in/simon-conrad/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <SiLinkedin />
+        </Link>
+        <Link
+          className="footer-link"
+          to="https://github.com/SlySkillet"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <SiGithub />
+        </Link>
+      </div>
     </div>
   );
 }
