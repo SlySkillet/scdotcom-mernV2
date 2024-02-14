b6eb93299be0160939217dd08bdf77904d03a4ec
Simon Conrad <simondbconrad@gmail.com>
2024-02-14 11:21:47 -0500

    finalize contact page

--- frontend/src/components/contact/Contact.jsx
+++ frontend/src/components/contact/Contact.jsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import { FaLinkedin, FaGithubAlt } from "react-icons/fa";
+import "./contact-styles.css";
 
 function Contact() {
   return (
@@ -8,40 +9,37 @@ function Contact() {
         <h2 className="contact-title">Contact me</h2>
       </div>
       <div className="contact-info-container">
-        <div className="inner-contact-info-container">
-          <h3 className="contact-subtitle">Social:</h3>
-          <div className="contact-links-container">
-            <Link
-              to="https://www.linkedin.com/in/simon-conrad/"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="contact-button"
-            >
-              LinkedIn <FaLinkedin />
-            </Link>
-            <Link
-              to="https://github.com/SlySkillet"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="contact-button"
-            >
-              GitHub <FaGithubAlt />
-            </Link>
-          </div>
-          <h3 className="contact-subtitle">Email:</h3>
-          <p className="contact-email" to="">
-            simon@simonconrad.com
+        <h3 className="contact-subtitle">social:</h3>
+        <div className="contact-links-container">
+          <Link
+            to="https://www.linkedin.com/in/simon-conrad/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="contact-button"
+          >
+            LinkedIn <FaLinkedin className="button-icon" />
+          </Link>
+          <Link
+            to="https://github.com/SlySkillet"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="contact-button"
+          >
+            GitHub <FaGithubAlt className="button-icon" />
+          </Link>
+        </div>
+        <h3 className="contact-subtitle">email:</h3>
+        <p className="contact-email" to="">
+          simon@simonconrad.com
+        </p>
+        <h3 className="contact-subtitle">discord:</h3>
+        <p className="contact-email">SlySkillet#1031</p>
+        <div className="contact-summary">
+          <p className="contact-detail">
+            I invite you to reach out on any of the above platforms and view my
+            open-source code on github. I am open to collaborate and open to
+            work. Cheers!{" "}
           </p>
-          <h3 className="contact-subtitle">Discord:</h3>
-          <p className="contact-email">SlySkillet#1031</p>
-          <div className="contact-summary">
-            <p className="contact-detail">
-              I am active on all of the above platforms. Please connect with me
-              on LinkedIn and reach out by whatever method you prefer. I am open
-              to collaborate on projects and open for work. Looking forward to
-              hearing from you. Cheers!{" "}
-            </p>
-          </div>
         </div>
       </div>
     </div>
